2c20c4a9869c9ac688b031c63dd74a09f563f2ae
flawas <32436687+flawas@users.noreply.github.com>
2023-02-19 15:19:55 +0100

    Berechnung von GGT Iterativ und Rekursiv

--- src/main/java/ch/hslu/ad/sw01/E1/ggt.java
+++ src/main/java/ch/hslu/ad/sw01/E1/ggt.java
@@ -1,8 +1,5 @@
 package ch.hslu.ad.sw01.E1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class ggt {
 
     public static void main(String[] args) {
