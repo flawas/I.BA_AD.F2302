8fc0f42e720be30280ac806a5d86fd794a1fc43d
flawas <32436687+flawas@users.noreply.github.com>
2023-02-19 15:19:42 +0100

    Berechnung von GGT Iterativ und Rekursiv

---
+++ src/main/java/ch/hslu/ad/sw01/E1/ggt.java
@@ -0,0 +1,55 @@
+package ch.hslu.ad.sw01.E1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ggt {
+
+    public static void main(String[] args) {
+        int a = 16;
+        int b = 68;
+
+        System.out.println("a: " + a);
+        System.out.println("a: " + b);
+        System.out.println("ggtIterativ1: " + ggtIterativ1(a, b));
+        System.out.println("ggtIterativ2: " + ggtIterativ2(a, b));
+        System.out.println("ggtRekursiv: "  + ggtRekursiv(a, b));
+
+
+    }
+
+    public static int ggtIterativ1(int a, int b) {
+        while (a != b) {
+            if (a > b) { a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static int ggtIterativ2(int a, int b) {
+        while ((a != 0) && (b != 0)) {
+            if (a > b) { a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        return (a + b); // Entweder a oder b ist 0!
+    }
+
+    public static int ggtRekursiv(final int a, final int b) {
+        if (a > b) {
+        return ggtRekursiv(a - b, b);
+        } else {
+            if (a < b) {
+                return ggtRekursiv(a, b - a);
+            } else {
+                return a;
+            }
+        }
+    }
+
+
+}
+
