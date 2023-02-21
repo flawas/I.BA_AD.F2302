464cc63bbb6b09c542b9513dd94803e23adeb642
flawas <32436687+flawas@users.noreply.github.com>
2023-02-21 09:10:36 +0100

    Überarbeitung

--- src/main/java/ch/hslu/ad/sw01/E1/ggt.java
+++ src/main/java/ch/hslu/ad/sw01/E1/ggt.java
@@ -10,14 +10,15 @@ public class ggt {
         System.out.println("a: " + b);
         System.out.println("ggtIterativ1: " + ggtIterativ1(a, b));
         System.out.println("ggtIterativ2: " + ggtIterativ2(a, b));
-        System.out.println("ggtRekursiv: "  + ggtRekursiv(a, b));
+        System.out.println("ggtRekursiv: " + ggtRekursiv(a, b));
 
 
     }
 
     public static int ggtIterativ1(int a, int b) {
         while (a != b) {
-            if (a > b) { a = a - b;
+            if (a > b) {
+                a = a - b;
             } else {
                 b = b - a;
             }
@@ -27,7 +28,8 @@ public class ggt {
 
     public static int ggtIterativ2(int a, int b) {
         while ((a != 0) && (b != 0)) {
-            if (a > b) { a = a % b;
+            if (a > b) {
+                a = a % b;
             } else {
                 b = b % a;
             }
@@ -37,7 +39,7 @@ public class ggt {
 
     public static int ggtRekursiv(final int a, final int b) {
         if (a > b) {
-        return ggtRekursiv(a - b, b);
+            return ggtRekursiv(a - b, b);
         } else {
             if (a < b) {
                 return ggtRekursiv(a, b - a);
@@ -46,7 +48,5 @@ public class ggt {
             }
         }
     }
-
-
 }
 
