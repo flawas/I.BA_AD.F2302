89c85d5f94dc3518c533cabf60a39bbe1e2c00b0
flawas <32436687+flawas@users.noreply.github.com>
2023-04-23 17:03:40 +0200

    SW09: Sortierung

---
+++ src/main/java/ch/hslu/ad/sw09/directEntry/Sort.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright Flavio Waser (c) 2023.
+ */
+
+package ch.hslu.ad.sw09.directEntry;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+
+/**
+ * Übung: Grundlagen, einfache Sortieralgorithmen (A1)
+ * Aufgabe: Direktes Einfügen
+ *
+ * @author Fabian Gröger
+ * @version 25.04.2018
+ */
+public class Sort {
+
+    /**
+     * Sortiert das int‐Array aufsteigend, mit direktem Einfügen (insertion sort)
+     *
+     * @param a Zu sortierendes Array.
+     */
+    public static void insertionSort(final Integer[] a) {
+        int elt;
+        int j;
+
+        // Initialize a new array that is bigger than the given
+        // the new array will be used to store the dummy element on the first position
+        Integer[] dummyArray = new Integer[a.length + 1];
+        System.arraycopy(a, 0, dummyArray, 1, a.length);
+
+        for (int i = 2; i < dummyArray.length; i++) {
+            elt = dummyArray[i]; // next elt for insert
+            dummyArray[0] = elt; // dummy - element
+            j = i; // a[1] ... a[j-1] already sorted
+
+            while (dummyArray[j - 1] > elt) {
+                dummyArray[j] = dummyArray[j - 1]; // shift right
+                j--; // go further left
+            }
+
+            dummyArray[j] = elt; // insert elt
+        } // a[1] ... a[j] sorted
+
+        System.arraycopy(dummyArray, 1, a, 0, a.length); // copy array back to source array
+    }
+
+    /**
+     * Sortiert das int-Array aufsteigend, mit direktem Einfügen (insertion sort)
+     * Suche nach der nächsten freien stelle wird mit binärer Suche realisiert
+     *
+     * @param a Zu sortierendes Array.
+     */
+    public static void binaryInsertionSort(final Integer[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int x = a[i];
+
+            // Find location to insert using binary search
+            int j = Math.abs(Arrays.binarySearch(a, 0, i, x) + 1);
+
+            //Shifting array to one location right
+            System.arraycopy(a, j, a, j + 1, i - j);
+
+            //Placing element at its correct location
+            a[j] = x;
+        }
+    }
+
+    /**
+     * Sortiert das int-Array aufsteigend, mit direktem Auswählen (selection sort)
+     *
+     * @param a Zu sortierendes Array.
+     */
+    public static void selectionSort(final Integer[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int index = i;
+
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[index])
+                    index = j;
+            }
+
+            int smallerNumber = a[index];
+            a[index] = a[i];
+            a[i] = smallerNumber;
+        }
+    }
+
+    /**
+     * Sortiert das int-Array aufsteigend, mit direktem Austauschen (bubble sort)
+     *
+     * @param a Zu sortierendes Array.
+     */
+    public static void bubbleSort(final Integer[] a) {
+        int n = a.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * Sortiert das int-Array aufsteigend, mit shell sort
+     *
+     * @param a Zu sortierendes Array.
+     */
+    public static void shellSort(final Integer[] a) {
+        int n = a.length;
+
+        // Start with a big gap, then reduce the gap
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            // Do a gapped insertion sort for this gap size.
+            // The first gap elements a[0..gap-1] are already
+            // in gapped order keep adding one more element
+            // until the entire array is gap sorted
+            for (int i = gap; i < n; i += 1) {
+                // add a[i] to the elements that have been gap
+                // sorted save a[i] in temp and make a hole at
+                // position i
+                int temp = a[i];
+
+                // shift earlier gap-sorted elements up until
+                // the correct location for a[i] is found
+                int j;
+                for (j = i; j >= gap && a[j - gap] > temp; j -= gap) {
+                    a[j] = a[j - gap];
+                }
+
+                // put temp (the original a[i]) in its correct
+                // location
+                a[j] = temp;
+            }
+        }
+    }
+
+    /**
+     * Sortiert das int-Array aufsteigend, mit shell sort
+     *
+     * @param a Zu sortierendes Array.
+     */
+    public static void shellSort(final int[] a) {
+        int n = a.length;
+
+        // Start with a big gap, then reduce the gap
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            // Do a gapped insertion sort for this gap size.
+            // The first gap elements a[0..gap-1] are already
+            // in gapped order keep adding one more element
+            // until the entire array is gap sorted
+            for (int i = gap; i < n; i += 1) {
+                // add a[i] to the elements that have been gap
+                // sorted save a[i] in temp and make a hole at
+                // position i
+                int temp = a[i];
+
+                // shift earlier gap-sorted elements up until
+                // the correct location for a[i] is found
+                int j;
+                for (j = i; j >= gap && a[j - gap] > temp; j -= gap) {
+                    a[j] = a[j - gap];
+                }
+
+                // put temp (the original a[i]) in its correct
+                // location
+                a[j] = temp;
+            }
+        }
+    }
+}
---
+++ src/test/java/ch/hslu/ad/sw09/directEntry/SortTest.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright Flavio Waser (c) 2023.
+ */
+
+package ch.hslu.ad.sw09.directEntry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Übung: Grundlagen, einfache Sortieralgorithmen (A1)
+ * Aufgabe: Direktes Einfügen
+ *
+ * @author Fabian Gröger
+ * @version 25.04.2018
+ */
+public class SortTest {
+
+    private static final int ARRAY_SIZE = 50000;
+    private static final Logger LOG = LogManager.getLogger(SortTest.class);
+
+    private Integer[] bigArraySorted;
+    private Integer[] bigArrayReverseSorted;
+    private Integer[] bigArrayUnsorted;
+
+    private long start;
+    private long end;
+
+    @Before
+    public void setup() {
+        bigArraySorted = new Integer[ARRAY_SIZE];
+        bigArrayReverseSorted = new Integer[ARRAY_SIZE];
+        bigArrayUnsorted = new Integer[ARRAY_SIZE];
+
+        Random random = new Random();
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            bigArraySorted[i] = i;
+            bigArrayReverseSorted[i] = ARRAY_SIZE - i;
+            bigArrayUnsorted[i] = random.nextInt(ARRAY_SIZE * 10);
+        }
+    }
+
+    @Test
+    public void testInsertionSortTime() {
+        // test with a big array sorted
+        start = System.currentTimeMillis();
+        Sort.insertionSort(bigArraySorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Insertion sort - Big array sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArraySorted, Order.ASCENDING);
+
+        // test with a big array reverse sorted
+        start = System.currentTimeMillis();
+        Sort.insertionSort(bigArrayReverseSorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Insertion sort - Big array reverse sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayReverseSorted, Order.ASCENDING);
+
+        // test with a big array unsorted
+        start = System.currentTimeMillis();
+        Sort.insertionSort(bigArrayUnsorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Insertion sort - Big array unsorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayUnsorted, Order.ASCENDING);
+    }
+
+    @Test
+    public void testBinaryInsertionSortTime() {
+        // test with a big array sorted
+        start = System.currentTimeMillis();
+        Sort.binaryInsertionSort(bigArraySorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Binary Insertion sort - Big array sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArraySorted, Order.ASCENDING);
+
+        // test with a big array reverse sorted
+        start = System.currentTimeMillis();
+        Sort.binaryInsertionSort(bigArrayReverseSorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Binary Insertion sort - Big array reverse sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayReverseSorted, Order.ASCENDING);
+
+        // test with a big array unsorted
+        start = System.currentTimeMillis();
+        Sort.binaryInsertionSort(bigArrayUnsorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Binary Insertion sort - Big array unsorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayUnsorted, Order.ASCENDING);
+    }
+
+    @Test
+    public void testSelectionSortTime() {
+        // test with a big array sorted
+        start = System.currentTimeMillis();
+        Sort.selectionSort(bigArraySorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Selection sort - Big array sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArraySorted, Order.ASCENDING);
+
+        // test with a big array reverse sorted
+        start = System.currentTimeMillis();
+        Sort.selectionSort(bigArrayReverseSorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Selection sort - Big array reverse sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayReverseSorted, Order.ASCENDING);
+
+        // test with a big array unsorted
+        start = System.currentTimeMillis();
+        Sort.selectionSort(bigArrayUnsorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Selection sort - Big array unsorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayUnsorted, Order.ASCENDING);
+    }
+
+    @Test
+    public void testBubbleSortTime() {
+        // test with a big array sorted
+        start = System.currentTimeMillis();
+        Sort.bubbleSort(bigArraySorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Bubble sort - Big array sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArraySorted, Order.ASCENDING);
+
+        // test with a big array reverse sorted
+        start = System.currentTimeMillis();
+        Sort.bubbleSort(bigArrayReverseSorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Bubble sort - Big array reverse sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayReverseSorted, Order.ASCENDING);
+
+        // test with a big array unsorted
+        start = System.currentTimeMillis();
+        Sort.bubbleSort(bigArrayUnsorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Bubble sort - Big array unsorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayUnsorted, Order.ASCENDING);
+    }
+
+    @Test
+    public void testShellSortTime() {
+        // test with a big array sorted
+        start = System.currentTimeMillis();
+        Sort.shellSort(bigArraySorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Shell sort - Big array sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArraySorted, Order.ASCENDING);
+
+        // test with a big array reverse sorted
+        start = System.currentTimeMillis();
+        Sort.shellSort(bigArrayReverseSorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Shell sort - Big array reverse sorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayReverseSorted, Order.ASCENDING);
+
+        // test with a big array unsorted
+        start = System.currentTimeMillis();
+        Sort.shellSort(bigArrayUnsorted);
+        end = System.currentTimeMillis();
+
+        LOG.info("Shell sort - Big array unsorted: " + (end - start));
+        // check if array was correctly sorted
+        assertIsSorted(bigArrayUnsorted, Order.ASCENDING);
+    }
+
+    public enum Order {
+        ASCENDING, DESCENDING
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> boolean assertIsSorted(Comparable<T> a[], Order order) {
+        for (int i = 0; i < a.length - 1; i++) {
+            Comparable<T> current = a[i];
+            Comparable<T> next = a[i + 1];
+
+            int result = current.compareTo((T) next);
+            switch (order) {
+                case ASCENDING:
+                    assertFalse(result > 0);
+                    break;
+                case DESCENDING:
+                    assertFalse(result < 0);
+                    break;
+            }
+        }
+
+        return true;
+    }
+}
